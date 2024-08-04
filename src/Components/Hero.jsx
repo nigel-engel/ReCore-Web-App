0bcd8a97fc43733a1aef863b8495e777325fcf6f
Nigel Engel <75956966+nigel-engel@users.noreply.github.com>
2024-08-04 19:38:38 +1000

    feat: hero component
    add: hero section to website design

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,9 +1,12 @@
 export default function Hero() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-center text-blue-500 underline">
-        Hello world!
+    <div className="max-w-[1000px] mx-auto lg:text-center">
+      <h1 className="text-slate-50 text-4xl font-bold lg:text-6xl  ">
+      Build emails even faster with components constructed on top of React and Tailwind.
       </h1>
+      <p className="lg:text-2xl text-xl mt-5 lg:px-20 text-slate-400">
+      ReCore is a Design Library built for marketing and dev teams to streamline their email development process using:
+      </p>
     </div>
   )
 }
