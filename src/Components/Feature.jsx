84d753054b58f61354b5d8efc64e202b6a82e752
Nigel Engel <75956966+nigel-engel@users.noreply.github.com>
2024-08-05 16:50:03 +1000

    style: CSS Changes

--- src/Components/Feature.jsx
+++ src/Components/Feature.jsx
@@ -8,21 +8,23 @@ export default function Feature() {
   const li3 = "Brand Consistency and Quick Customization"
 
   return (
-    <div className="px-6 max-w-[1000px] mx-auto mt-6 grid md:grid-cols-2">
+    <div className="px-6 max-w-[1200px] mx-auto mt-20 gap-10 grid md:grid-cols-2">
       <div>
         <div className="inline-flex text-center px-4 py-1 text-sm bg-purple-600/30 rounded-md ">
           <p className="--pretitle text-purple-300">{pretitle}</p>
         </div>
-      <h2 className="--h2title my-2">{title}</h2>
-      <p className="--para mt-2">{subtitle}</p>
-      <ul className="--para --li mt-4 space-y-4">
-        <li><i class="bi bi-box-fill --li-icon"></i>{li1}</li>
-        <li><i class="bi bi-eyedropper --li-icon"></i>{li2}</li>
-        <li><i class="bi bi-arrow-repeat --li-icon"></i> {li3}</li>
-      </ul>
+        <h2 className="--h2title my-4">{title}</h2>
+        <p className="--para mt-2">{subtitle}</p>
+        <ul className=" --li mt-4 space-y-2">
+          <li><i class="bi bi-box-fill --li-icon"></i>{li1}</li>
+          <li><i class="bi bi-eyedropper --li-icon"></i>{li2}</li>
+          <li><i class="bi bi-arrow-repeat --li-icon"></i> {li3}</li>
+        </ul>
       </div>
       
-      <div className="bg-blue-500"></div>
+      <div >
+        <img src="/src/assets/figma.png" alt="" />
+      </div>
     </div>
   )
 }
